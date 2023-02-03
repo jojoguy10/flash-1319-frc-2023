d81110bb980e47b9346431391924d5ed0df9e473
Joe Kelly <jojoguy10@gmail.com>
2023-02-02 19:21:56 -0500

    MOVE TO ARCHIVE

--- 2023 test robot 2/test robot 2/src/main/java/frc/robot/RobotContainer.java
+++ 2023 test robot 2/test robot 2/src/main/java/frc/robot/RobotContainer.java
@@ -4,31 +4,22 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-// import frc.robot.commands.CommandArm;
 import frc.robot.commands.CommandDriveBase;
-// import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveBaseSubsystem;
 
 public class RobotContainer {
-  private DriveBaseSubsystem m_DriveBaseSubsystem = new DriveBaseSubsystem();
-  // private ArmSubsystem m_ArmSubsystem = new ArmSubsystem();
+  private final DriveBaseSubsystem m_DriveBaseSubsystem = new DriveBaseSubsystem();
   private final XboxController m_Driver = new XboxController(0);
-  //private final XboxController m_Operator = new XboxController(0);
   private final JoystickButton m_Driver_b = new JoystickButton(m_Driver, XboxController.Button.kB.value);
   private final JoystickButton m_Driver_y= new JoystickButton(m_Driver, XboxController.Button.kY.value);
 
-  //private final JoystickButton m_Operator_x= new JoystickButton(m_Operator, XboxController.Button.kX.value);
-
   public RobotContainer() {
-    m_DriveBaseSubsystem.setDefaultCommand(new CommandDriveBase(m_DriveBaseSubsystem, m_Driver));
-  //  m_Operator_x.onTrue(new CommandArm(m_ArmSubsystem));
-  //   m_Driver_b.onTrue(new CommandDriveBase(m_DriveBaseSubsystem, m_Driver));
-  //  m_Driver_y.onTrue(new CommandDriveBase(m_DriveBaseSubsystem, m_Driver));
- }
-	public Command getAutonomousCommand() {
+    final CommandDriveBase driveBaseCommand = new CommandDriveBase(m_DriveBaseSubsystem, m_Driver);
+    m_DriveBaseSubsystem.setDefaultCommand(driveBaseCommand);
+  }
+	
+  public Command getAutonomousCommand() {
 		return null;
 	}
-
- 
 }
 
--- 2023 test robot 2/test robot 2/src/main/java/frc/robot/commands/CommandDriveBase.java
+++ 2023 test robot 2/test robot 2/src/main/java/frc/robot/commands/CommandDriveBase.java
@@ -4,15 +4,15 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveBaseSubsystem;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-
 public class CommandDriveBase extends CommandBase{
     private DriveBaseSubsystem m_Base;
     private XboxController m_XboxController;
+    
     public CommandDriveBase(DriveBaseSubsystem base, XboxController xbox){
         m_Base = base;
         m_XboxController = xbox;
     }
+
     public void execute(){
         double x;
         double y;
