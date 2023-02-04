1d1bbb9ca1abbb5de2712c36427cf8aaa0de9aa1
PeterGargas <gargaspeter1@gmail.com>
2023-02-04 15:54:55 -0500

    competition robot

--- 2023 test robot/src/main/java/frc/robot/Robot.java
+++ 2023 test robot/src/main/java/frc/robot/Robot.java
@@ -6,9 +6,12 @@ package frc.robot;
 
 import javax.print.attribute.standard.Compression;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -27,10 +30,20 @@ import edu.wpi.first.wpilibj.util.WPILibVersion;
  */
 public class Robot extends TimedRobot {
 
+  private final Double COUNTS_PER_INCH = 841.0;
+  private final Double INCHES_PER_COUNT = 0.0012;
+
   private final WPI_TalonFX m_leftMotor1 = new WPI_TalonFX(1);
   private final WPI_TalonFX m_leftMotor2 = new WPI_TalonFX(2);
   private final WPI_TalonFX m_rightMotor1 = new WPI_TalonFX(3);
   private final WPI_TalonFX m_rightMotor2 = new WPI_TalonFX(4);
+  private final WPI_TalonFX spinerMotor1 = new WPI_TalonFX(9);
+  private final WPI_TalonFX spinerMotor2 = new WPI_TalonFX(10);
+  private final WPI_TalonFX intake = new WPI_TalonFX(6);
+  private final WPI_TalonFX intake2 = new WPI_TalonFX(7);
+
+  private final WPI_TalonSRX levelsMotor = new WPI_TalonSRX(12);
+  private final WPI_TalonSRX theTurret = new WPI_TalonSRX(8);
 
   private DifferentialDrive m_robotDrive ;
 
@@ -42,7 +55,9 @@ public class Robot extends TimedRobot {
  
   private boolean shift = false; 
 
+  private double autoSpeed = 0.25;
 
+  private final DigitalInput bottomlimitSwitch = new DigitalInput(4);
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -56,6 +71,9 @@ public class Robot extends TimedRobot {
     m_rightMotor1.setInverted(true);
     m_rightMotor2.setInverted(true);
 
+    spinerMotor2.setInverted(true);
+    intake.setInverted(true);
+
 
     m_leftMotor2.follow(m_leftMotor1);
     m_rightMotor2.follow(m_rightMotor1);
@@ -64,6 +82,10 @@ public class Robot extends TimedRobot {
 
     m_Compressor.enableDigital();
 
+    m_leftMotor1.setSelectedSensorPosition(0,0,0);
+    m_leftMotor2.setSelectedSensorPosition(0,0,0);
+    m_rightMotor1.setSelectedSensorPosition(0,0,0);
+    m_rightMotor2.setSelectedSensorPosition(0,0,0);
 
   }
 
@@ -76,14 +98,19 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    m_robotDrive.arcadeDrive(m_Joystick.getRightX(),m_Joystick.getLeftY());
-    m_shifter.set(m_Joystick.getAButton());
-    if (m_Joystick.getRightBumperPressed()){
-      shift = false;
-    }else if (m_Joystick.getLeftBumperPressed()){
-      shift = true;
-    }
-    m_shifter.set(shift);
+    
+
+
+    SmartDashboard.putNumber("Joystick x Left value.", m_Joystick.getLeftX());
+    SmartDashboard.putNumber("Joystick x Right value.", m_Joystick.getRightX());
+    SmartDashboard.putNumber("Joystick y Right value.", m_Joystick.getRightY());
+    SmartDashboard.putNumber("Joystick y Left value.", m_Joystick.getLeftY());
+    SmartDashboard.putNumber("L motor 1 encoder.", m_leftMotor1.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("L motor 2  encoder.", m_leftMotor2.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("R motor 1  encoder.", m_rightMotor1.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("R motor 2 encoder.", m_rightMotor2.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("levels Motor", levelsMotor.getSelectedSensorPosition(0));
+
   }
 
   /**
@@ -97,11 +124,30 @@ public class Robot extends TimedRobot {
    * chooser code above as well.
    */
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    m_leftMotor1.setSelectedSensorPosition(0,0,0);
+    m_leftMotor2.setSelectedSensorPosition(0,0,0);
+    m_rightMotor1.setSelectedSensorPosition(0,0,0);
+    m_rightMotor2.setSelectedSensorPosition(0,0,0);
+  }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    SmartDashboard.putNumber("Auto speed ", autoSpeed);
+     if (m_leftMotor1.getSelectedSensorPosition(0) >= 40368 ){
+      autoSpeed = 0;
+     }else{
+      autoSpeed = 0.25;
+     }
+     //m_robotDrive.arcadeDrive(autoSpeed, 0);
+     m_leftMotor1.set(ControlMode.PercentOutput, autoSpeed);
+     m_leftMotor2.set(ControlMode.PercentOutput, autoSpeed);
+     m_rightMotor1.set(ControlMode.PercentOutput, -autoSpeed);
+     m_rightMotor2.set(ControlMode.PercentOutput, -autoSpeed);
+
+
+  }
 
   /** This function is called once when teleop is enabled. */
   @Override
@@ -109,7 +155,68 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_robotDrive.arcadeDrive(m_Joystick.getRightX(),m_Joystick.getLeftY());
+    m_shifter.set(m_Joystick.getAButton());
+    if (m_Joystick.getRightBumperPressed()){
+      shift = false;
+    }else if (m_Joystick.getLeftBumperPressed()){
+      shift = true;
+    }
+    m_shifter.set(shift);
+
+    
+    if (m_Joystick.getAButton()){
+      //levelsMotor.set(ControlMode.PercentOutput, 0.25);
+      setMotorSpeed(0.25);
+
+    }else if (m_Joystick.getYButton()){
+      //levelsMotor.set(ControlMode.PercentOutput, 0);
+      setMotorSpeed(-0.25);
+    }else{
+      setMotorSpeed(0);
+    }
+
+    if (m_Joystick.getXButton()){
+      theTurret.set(0.25);
+    } 
+    else if(m_Joystick.getBButton()){
+      theTurret.set(-0.25);
+    }else{
+      theTurret.set(0);
+    }
+
+    if(m_Joystick.getStartButton()){
+      spinerMotor1.set(0.50);
+      spinerMotor2.set(0.50);
+      intake.set(1);
+      intake2.set(1);
+    }else{
+      spinerMotor1.set(0);
+      spinerMotor2.set(0);
+      intake.set(0);
+      intake2.set(0);
+    }
+
+  }
+
+  public void setMotorSpeed(double speed) {
+    if (speed > 0) {
+      if (!bottomlimitSwitch.get()) {
+      levelsMotor.set(0);
+      
+      }else{
+        levelsMotor.set(speed);
+      }
+    }else if(speed<0){
+      levelsMotor.set(speed);
+    }else{
+      levelsMotor.set(0);
+    }
+
+  }
+
+    
 
   /** This function is called once when the robot is disabled. */
   @Override
